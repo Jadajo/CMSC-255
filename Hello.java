189d15fb7737336098618589b2403ee519c58c1a
Jadajo <112006205+Jadajo@users.noreply.github.com>
2022-08-24 12:40:25 -0400

    Add file via upload
    I just created my first lab assignment.

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/******************************************************
+ * Java program name
+ ******************************************************
+ * 	Jada Jones
+ * 08/24/2022
+ * CMSC 255
+ * ***************************************************/
+ 
+
+ public class Hello {
+ 		public static void main(String[] args) {
+
+ 			System.out.println("<Jada Jones>") ;
+ 			System.out.println("<Hello.java>") ;
+ 			System.out.println("<This program is a lab assignment that displays my name, program, and class info.>") ;
+ 			System.out.println("<08/24/2022>") ;
+ 			System.out.println("<CMSC 255>") ;
+ 			System.out.println() ;
+
+ 		}
+ }
